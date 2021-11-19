1f153d527f44007172c30d6dd69d63157c0e0158
JB <wotjr5477@naver.com>
2021-11-19 18:27:03 +0900

    211119-1

---
+++ 1회차/debug4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sum(int i);
+
+int main()
+{
+	int s;
+	s = sum(10);
+	printf("%d\n", s);
+}
+
+int sum(int i)
+{
+	if(i <= 0)
+		return 0;
+	else
+		return i + sum(i-1);
+}
